599244b90aa9b5296138fbcb390b156af39eb653
Mat Krol <43745362+mat-krol@users.noreply.github.com>
2022-04-21 23:52:42 +0200

    include theme in the app file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+
 import "typeface-roboto";
 import "typeface-antic-didone";
+import "../src/common/globals.css";
+import { theme } from "../src/common/theme";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
