a2fcd455f33afcfc26a7de197cafe0b70543f51d
Mat Krol <43745362+mat-krol@users.noreply.github.com>
2022-04-21 23:52:15 +0200

    set up the IndexView

---
+++ src/views/IndexView.tsx
@@ -0,0 +1,56 @@
+import { Box, Grid, Heading, Flex, Text } from "@chakra-ui/react";
+import Image from "next/image";
+
+export function IndexView() {
+  return (
+    <Box
+      paddingX={{ base: 4, lg: 0 }}
+      paddingY={{ base: 0, lg: 16 }}
+      marginY={8}
+      position="relative"
+      maxW="960px"
+      marginX="auto"
+    >
+      <Grid columnGap={16} templateColumns={{ base: "1fr", lg: "1fr 1fr" }}>
+        <Image src="/ania_pose_1.jpeg" width="500px" height="500px" />
+
+        <Flex
+          paddingRight={8}
+          direction="column"
+          justifyContent="space-between"
+        >
+          <Heading
+            as="h1"
+            size="4xl"
+            fontWeight="normal"
+            fontFamily="Antic Didone"
+            lineHeight="0.95"
+            letterSpacing="-2px"
+            marginY={{ base: 4, lg: 0 }}
+          >
+            Lorem Ipsum Dolor Sit Amet
+          </Heading>
+          <Text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dui
+            elit, tempus sit amet est at, dignissim fermentum justo. Maecenas
+            vitae faucibus mauris. Quisque lobortis purus lectus, at efficitur
+            sapien posuere quis. Nunc et nisi eget orci auctor venenatis. Fusce
+            tempus nisl sed ex viverra, a tempor quam sollicitudin. Morbi vel
+            leo ligula
+          </Text>
+        </Flex>
+      </Grid>
+      <Box
+        position="absolute"
+        borderColor="beige.700"
+        display={{ base: "none", lg: "block" }}
+        borderWidth="1px"
+        borderStyle="solid"
+        width="calc(100% - 72px)"
+        height="100%"
+        top="0"
+        left="72px"
+      />
+    </Box>
+  );
+}
