0310281dfdf3a9ad2ce1c66f3453dc0f3fb41a53
Mat Krol <43745362+mat-krol@users.noreply.github.com>
2022-04-26 00:20:35 +0200

    update common theme to teal

--- src/common/theme.ts
+++ src/common/theme.ts
@@ -2,27 +2,41 @@ import { extendTheme } from "@chakra-ui/react";
 
 export const theme = extendTheme({
   colors: {
-    beige: {
-      50: "#f7efec",
-      700: "#805E6B",
+    teal: {
+      25: "#F4F8F7",
+      50: "#EAF1EF",
+
+      "100": "#60c4d4",
+      "200": "#5cbacc",
+      "300": "#56aab9",
+      "400": "#4e95a0",
+      "500": "#488188",
+      "600": "#488188",
+      "700": "#488188",
+      "800": "#488188",
+      "900": "#3d5d5f",
     },
   },
   styles: {
     global: () => ({
       body: {
-        backgroundColor: "beige.50",
+        backgroundColor: "teal.50",
         fontFamily: "Roboto",
-        color: "beige.700",
-      },
-      h1: {
-        fontFamily: "Antic Didone",
-      },
-      h2: {
-        fontFamily: "Antic Didone",
-      },
-      h3: {
-        fontFamily: "Antic Didone",
+        color: "teal.500",
       },
     }),
+  },
+  components: {
+    Button: { baseStyle: { borderRadius: 0 } },
+    Input: { baseStyle: { borderRadius: 0 } },
+    Heading: {
+      baseStyle: {
+        fontFamily: "Antic Didone",
+        fontWeight: "normal",
+        lineHeight: "0.95",
+        letterSpacing: "-2px",
+        marginY: { base: 4, lg: 0 },
+      },
+    },
   },
 });
