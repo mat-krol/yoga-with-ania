cb92abeb1b4c60715ae963984e14599a0e6c5196
Mat Krol <43745362+mat-krol@users.noreply.github.com>
2022-04-27 00:33:45 +0200

    set up about page

---
+++ pages/about.tsx
@@ -0,0 +1,16 @@
+import { Footer } from "../src/components/Footer";
+import { HeaderWithNavigation } from "../src/components/HeaderWithNavigation";
+import { Layout } from "../src/components/Layout";
+import { AboutView } from "../src/views/AboutView";
+
+export default function AboutPage() {
+  return (
+    <Layout>
+      {{
+        header: <HeaderWithNavigation />,
+        body: <AboutView />,
+        footer: <Footer />,
+      }}
+    </Layout>
+  );
+}
