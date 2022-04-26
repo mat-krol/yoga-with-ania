b803d8720f8433511b3641e290680c48fcde747a
Mat Krol <43745362+mat-krol@users.noreply.github.com>
2022-04-27 00:34:39 +0200

    fix HeaderWithNav on mobiles

--- src/components/HeaderWithNavigation.tsx
+++ src/components/HeaderWithNavigation.tsx
@@ -1,51 +1,72 @@
-import { Button, IconButton, useBreakpointValue } from "@chakra-ui/react";
+import {
+  Button,
+  IconButton,
+  Stack,
+  Flex,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import Link from "next/link";
 import { FaInstagram } from "react-icons/fa";
 import {
   linkToAbout,
   linkToContact,
-  linkToIndex,
   linkToPractiseWithMe,
 } from "../common/links";
 
 import { Header } from "./Header";
 
-export function HeaderWithNavigation() {
-  const showOnMobile = useBreakpointValue({ base: true, md: false });
+const links = [
+  { title: "Practise with me", link: linkToPractiseWithMe() },
+  { title: "About", link: linkToAbout() },
+  {
+    title: "Contact",
+    link: linkToContact(),
+  },
+  { icon: FaInstagram, link: "https://www.instagram.com/yogawithania_" },
+];
 
-  const links = [
-    { title: "Practise with me", link: linkToPractiseWithMe() },
-    { title: "About", link: linkToAbout() },
-    {
-      title: "Contact",
-      link: linkToContact(),
-    },
-    { icon: FaInstagram, link: linkToIndex() },
-  ];
+export function HeaderWithNavigation() {
+  const isMobile = useBreakpointValue({ base: true, md: false });
 
   return (
     <Header>
-      {!showOnMobile && (
-        <>
-          {links.map(({ title, link, icon: Icon }) => (
-            <Link href={link} key={title}>
-              {Icon ? (
-                <IconButton
-                  color="teal.800"
-                  aria-label="Instagram"
-                  variant="link"
-                  justifyContent="end"
-                  icon={<Icon />}
-                />
-              ) : (
-                <Button colorScheme="teal" fontWeight="normal" variant="link">
-                  {title}
-                </Button>
-              )}
-            </Link>
-          ))}
-        </>
+      {isMobile ? (
+        <Stack align="end" marginTop={4} spacing={0}>
+          <LinkList />
+        </Stack>
+      ) : (
+        <Flex alignItems="center" justifyContent="space-between">
+          <LinkList />
+        </Flex>
       )}
     </Header>
   );
 }
+
+const LinkList = () => (
+  <>
+    {links.map(({ title, link, icon: Icon }) => (
+      <Link href={link} key={title}>
+        {Icon ? (
+          <IconButton
+            color="teal.800"
+            aria-label="Instagram"
+            variant="link"
+            justifyContent="end"
+            icon={<Icon />}
+            paddingY={{ base: 2, md: "unset" }}
+          />
+        ) : (
+          <Button
+            colorScheme="teal"
+            fontWeight="normal"
+            variant="link"
+            paddingY={{ base: 2, md: "unset" }}
+          >
+            {title}
+          </Button>
+        )}
+      </Link>
+    ))}
+  </>
+);
