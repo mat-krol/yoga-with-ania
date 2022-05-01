c114fb435fd05c03e4796c549b2d6f4deedc9d2a
Mat Krol <43745362+mat-krol@users.noreply.github.com>
2022-05-01 04:51:54 +0100

    add new links

--- src/common/links.ts
+++ src/common/links.ts
@@ -1,6 +1,8 @@
 export const linkToIndex = () => "/";
 
 export const linkToPractiseWithMe = () => "/practise-with-me";
+export const linkToCurrentSchedule = () => "/current-schedule";
+export const linkToPrivatesAndCorporate = () => "/privates-and-corporate";
 export const linkToBlog = () => "/blog";
 export const linkToContact = () => "/contact";
 export const linkToAbout = () => "/about";
