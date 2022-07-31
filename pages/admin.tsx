de828e0d3e2ce08c5b34baa00dd38320a81a528b
Mat Krol <43745362+mat-krol@users.noreply.github.com>
2022-07-31 01:41:33 +0100

    fix ts errors

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -3,8 +3,10 @@ import dynamic from "next/dynamic";
 import { config } from "../cms/config";
 
 const CMS = dynamic(
+  // @ts-ignore
   () =>
     import("netlify-cms-app").then((cms) => {
+      // @ts-ignore
       cms.init({ config });
     }),
   { ssr: false, loading: () => <p>Loading...</p> }
